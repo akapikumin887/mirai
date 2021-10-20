81c5e7328bd6affbda1f0c9aeec6b00ef82f3521
ichikawa <m36479.ch11166@gmail.com>
2021-10-21 02:00:56 +0900

    敵の巡回作成 Ichikawaフォルダ作成

---
+++ futurecreate/Assets/Ichikawa/Script/Patrol.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//NavMeshAgent使うときに必要
+using UnityEngine.AI;
+
+//オブジェクトにNavMeshAgentコンポーネントを設置
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] points;
+    [SerializeField] int destPoint = 0;
+    private NavMeshAgent agent;
+
+    float distance;
+    [SerializeField] float trackingRange = 3f;
+    [SerializeField] float quitRange = 5f;
+    [SerializeField] bool tracking = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // autoBraking を無効にすると、目標地点の間を継続的に移動します
+        //(つまり、エージェントは目標地点に近づいても
+        // 速度をおとしません)
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // 地点がなにも設定されていないときに返します
+        if (points.Length == 0)
+            return;
+
+        // エージェントが現在設定された目標地点に行くように設定します
+        agent.destination = points[destPoint].position;
+
+        // 配列内の次の位置を目標地点に設定し、
+        // 必要ならば出発地点にもどります
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Update()
+    {
+        // エージェントが現目標地点に近づいてきたら、
+        // 次の目標地点を選択します
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        //trackingRangeの範囲を赤いワイヤーフレームで示す
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, trackingRange);
+
+        //quitRangeの範囲を青いワイヤーフレームで示す
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, quitRange);
+    }
+}
