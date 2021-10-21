b95d290dc186e293664ccff413669d04486952c2
razuberikku <50733194+razuberikku@users.noreply.github.com>
2021-10-21 13:14:48 +0900

    敵に範囲を追加

--- futurecreate/Assets/sasaki/sasaki/Enemymove.cs
+++ futurecreate/Assets/sasaki/sasaki/Enemymove.cs
@@ -20,7 +20,25 @@ public class Enemymove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ñ⁄ìIínÇê›íË
-        Player_Nav.SetDestination(Player.transform.position);
+        Vector3 Enemy_position = this.transform.position;
+        Vector3 Player_position = Player.transform.position;
+
+        Vector3 hit;
+        hit.x = Enemy_position.x - Player_position.x;
+        hit.y = Enemy_position.y - Player_position.y;
+        hit.z = Enemy_position.z - Player_position.z;
+
+        if(hit.x * hit.x + hit.y * hit.y < 50)
+        {
+            GetComponent<NavMeshAgent>().isStopped = false;
+
+            Player_Nav.SetDestination(Player.transform.position);
+        }
+        else
+        {
+            GetComponent<NavMeshAgent>().isStopped = true;
+            //Stopping distance
+            //NavMeshAgent.Stop(stopUpdates: true);
+        }
     }
 }
