2d7561bf504e6bbe4b352ac925de7224ee33c940
LAPTOP-E1IGLONJ\mcagn <mcagn@ahk.jp>
2021-10-21 11:43:34 +0900

    infinity修正

--- futurecreate/Assets/Isobe/Script/Danger.cs
+++ futurecreate/Assets/Isobe/Script/Danger.cs
@@ -7,24 +7,39 @@ using UnityEngine.AI;
 public class Danger : MonoBehaviour
 {
     private Text text;
-    private float alltime;
+    //private float alltime;
     [SerializeField] float danger_score;
     [SerializeField] GameObject enemy;
+    [SerializeField] GameObject player;
     // Start is called before the first frame update
 
     void Start()
     {
         text = this.GetComponent<Text>();
-        alltime = 0.0f;
+        //alltime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         //alltime += Time.deltaTime;
-        Debug.Log(Mathf.Sin(alltime));
-        danger_score = 100.0f-enemy.GetComponent<NavMeshAgent>().remainingDistance;   //リアルタイム変動
+        //Debug.Log(Mathf.Sin(alltime));
+
+        NavMeshPath path = enemy.GetComponent<NavMeshAgent>().path; //経路パス（曲がり角座標のVector3配列）を取得
+        float dist = 0f;
+        Vector3 corner = player.transform.position; //自分の現在位置
+                                            
+        for (int i = 0; i < path.corners.Length; i++) //曲がり角間の距離を累積していく
+        {
+            Vector3 corner2 = path.corners[i];
+            dist += Vector3.Distance(corner, corner2);
+            corner = corner2;
+
+        }
+
+        Debug.Log(dist);
+        danger_score = 100.0f-dist;   //リアルタイム変動
         danger_score = Mathf.Clamp(danger_score, 0.0f, 100.0f);
         Debug.Log(danger_score);
         text.text = "危険度:"+(int)danger_score+"%";                         //テキスト内容
