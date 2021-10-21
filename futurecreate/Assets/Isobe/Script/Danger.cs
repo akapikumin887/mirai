6c59cf95d5128fca494243cdada26f253ebc9f6e
LAPTOP-E1IGLONJ\mcagn <mcagn@ahk.jp>
2021-10-21 14:16:12 +0900

    危険度を直線距離に変更

--- futurecreate/Assets/Isobe/Script/Danger.cs
+++ futurecreate/Assets/Isobe/Script/Danger.cs
@@ -26,21 +26,24 @@ public class Danger : MonoBehaviour
         //alltime += Time.deltaTime;
         //Debug.Log(Mathf.Sin(alltime));
 
-        NavMeshPath path = enemy.GetComponent<NavMeshAgent>().path; //経路パス（曲がり角座標のVector3配列）を取得
-        float dist = 0f;
-        Vector3 corner = player.transform.position; //自分の現在位置
+        //NavMeshPath path = enemy.GetComponent<NavMeshAgent>().path; //経路パス（曲がり角座標のVector3配列）を取得
+        //float dist = 0f;//距離
+        //Vector3 corner = player.transform.position; //自分の現在位置
                                             
-        for (int i = 0; i < path.corners.Length; i++) //曲がり角間の距離を累積していく
-        {
-            Vector3 corner2 = path.corners[i];
-            dist += Vector3.Distance(corner, corner2);
-            corner = corner2;
+        //for (int i = 0; i < path.corners.Length; i++) //曲がり角間の距離を累積していく
+        //{
+        //    Vector3 corner2 = path.corners[i];//パスのポジション
+        //    dist += Vector3.Distance(corner, corner2);//距離加算
+        //    corner = corner2;
 
-        }
+        //}
 
-        Debug.Log(dist);
-        danger_score = 100.0f-dist;   //リアルタイム変動
+        //Debug.Log(dist);
+
+        danger_score = 5.0f/Vector3.Distance(enemy.transform.position,player.transform.position)*100.0f;   //リアルタイム変動
+
         danger_score = Mathf.Clamp(danger_score, 0.0f, 100.0f);
+    
         Debug.Log(danger_score);
         text.text = "危険度:"+(int)danger_score+"%";                         //テキスト内容
     }
