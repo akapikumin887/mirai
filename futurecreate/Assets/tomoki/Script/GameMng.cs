bc06e0efe7ad28eeccc5d9f86bd18a901bbf5014
aka <akapikumin232@gmail.com>
2021-11-01 19:20:44 +0900

    ともき : EnemyManagerをGameMngに変更をお願いします

--- futurecreate/Assets/tomoki/Script/GameMng.cs
+++ futurecreate/Assets/tomoki/Script/GameMng.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
+public class GameMng : MonoBehaviour
 {
     private List<GameObject> _Enemys = new List<GameObject>();
 
@@ -16,6 +16,7 @@ public class EnemyManager : MonoBehaviour
         foreach (var obj in objects)
             _Enemys.Add(obj);
     }
+
 
     void Start()
     {
@@ -36,7 +37,7 @@ public class EnemyManager : MonoBehaviour
         return true;
     }
 
-    public List<GameObject> GetEnemy(){ return _Enemys; }
+    public List<GameObject> GetEnemy() { return _Enemys; }
 
     public List<GameObject> GetVisibilityEnemy()
     {
@@ -59,7 +60,7 @@ public class EnemyManager : MonoBehaviour
         foreach (var obj in _Enemys)
         {
             var scr = obj.GetComponent<LisEnemy>();
-            if(scr != null)
+            if (scr != null)
                 lEnemy.Add(obj);
         }
 
