7cab33f2a6b0b029ab6148a8ef2ff8942738aa1b
razuberikku <50733194+razuberikku@users.noreply.github.com>
2021-11-11 10:59:58 +0900

    目がいい敵距離制限つけた

--- futurecreate/Assets/sasaki/sasaki/EnemyVisibility.cs
+++ futurecreate/Assets/sasaki/sasaki/EnemyVisibility.cs
@@ -34,24 +34,29 @@ public class EnemyVisibility : MonoBehaviour
         Vector3 Enemy_forward_direction = this.transform.forward;//敵の正面ベクトルを取得
         Vector3 Player_direction = (Player.transform.position - this.transform.position).normalized;     // プレイヤーの方向ベクトル
 
+
         //敵の視界の設定
         float dot = Vector3.Dot(Enemy_forward_direction, Player_direction);     //敵の前方ベクトルとプレイヤー方向とのベクトルの内積計算
         if (dot > 0.4f)     //内積
         {
             //Rayのせってい　壁かどうか判定するため
             RaycastHit hit;
-            if (Physics.Raycast(this.transform.position, Player_direction, out hit, Mathf.Infinity))     //Rayにあたるものがあった時
+
+
+            Debug.Log(Player_direction);
+
+            if (Physics.Raycast(this.transform.position, Player_direction , out hit, 6.0f))     //Rayにあたるものがあった時  最後の引数が距離
             {
                 if (hit.collider.tag == "wall")      //間に壁があるとき
                 {
-                    Debug.Log("壁がある");
+                    //Debug.Log("壁がある");
                     this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.PATROL);
 
                 }
                 if (hit.collider.tag == "Player")   //プレイヤーまで障害物がないとき
                 {
                                                                                   //  GetComponent<NavMeshAgent>().isStopped = false;               //ナビゲーションを使う
-                    Debug.Log("追っかけるよー");
+                    //Debug.Log("追っかけるよー");
                     this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.TRACKING);        //Enemyの行動パターンの変更　パトロールのパターンに変更
                     this.GetComponent<Enemy>().SetDestination(Player.transform.position);
                     //Player_Nav.SetDestination(Player.transform.position);       //ナビゲーションの目標座標 SetEnemyType(ENEMY_TYPE type)を呼び出す処理に変える
@@ -59,13 +64,13 @@ public class EnemyVisibility : MonoBehaviour
             }
             else//  目の前になにもないとき
             {
-                Debug.Log("探すよー");
+                //Debug.Log("探すよー");
                 this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.PATROL);
             }
         }
         else
         {
-            Debug.Log("何も見てないよ");
+            //Debug.Log("何も見てないよ");
             this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.PATROL);
         }
     }
