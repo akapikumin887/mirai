91c38b5f0b076681821f5f0ee25d411424f4276a
razuberikku <50733194+razuberikku@users.noreply.github.com>
2021-11-22 16:26:43 +0900

    透ける処理

--- futurecreate/Assets/Isobe/Script/CameraManager.cs
+++ futurecreate/Assets/Isobe/Script/CameraManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] GameObject Player;
+    Vector3 Center;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 Center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
     }
 
     // Update is called once per frame
@@ -16,5 +20,50 @@ public class CameraManager : MonoBehaviour
     {
         //簡易的な追従
         transform.position = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z-5.5f);
+        //Debug.Log(transform.position);
+
+        test();
+    }
+
+    void test ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Center);
+
+
+        RaycastHit hit;
+
+        Vector3 Player_direction = (Player.transform.position - this.transform.position).normalized;
+        Debug.DrawRay(Player.transform.position, Player_direction* 50, Color.red, 100000, true);
+
+        if (Physics.Raycast(this.transform.position, Player_direction, out hit, Mathf.Infinity))            //プレイヤーからカメラにレイを飛ばす
+        {
+            //GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+            Debug.Log(hit.collider.tag);
+
+
+            if (hit.collider.tag == "wall")
+            {
+                var a = hit.collider.tag;
+                hit.collider.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+                Debug.Log("atatta"); 
+            }
+
+            if (hit.collider.tag == "Player")
+            {
+                GameObject[] walls = GameObject.FindGameObjectsWithTag("wall");
+                foreach (GameObject wall in walls)
+                {
+                    //hit.collider.GetComponent<Renderer>().material.color = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+                    wall.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                }
+            }
+
+        }
+        else
+        {
+            //hit.collider.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+
     }
 }
