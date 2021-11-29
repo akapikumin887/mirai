ee656cb258bd37b374b54cae8dc94939f6e8d58d
razuberikku <50733194+razuberikku@users.noreply.github.com>
2021-11-29 19:36:18 +0900

    UIカードキー表示

--- futurecreate/Assets/sasaki/sasaki/EnemyVisibility.cs
+++ futurecreate/Assets/sasaki/sasaki/EnemyVisibility.cs
@@ -23,7 +23,6 @@ public class EnemyVisibility : MonoBehaviour
 
         // 目的地のオブジェクトを取得
         Player = game_mng.GetPlayer();
-        Debug.Log("");
     }
 
     // Update is called once per frame
@@ -42,36 +41,26 @@ public class EnemyVisibility : MonoBehaviour
             //Rayのせってい　壁かどうか判定するため
             RaycastHit hit;
 
-
-           // Debug.Log(Player_direction);
-
             if (Physics.Raycast(this.transform.position, Player_direction , out hit, 6.0f))     //Rayにあたるものがあった時  最後の引数が距離
             {
-                if (hit.collider.tag == "wall")      //間に壁があるとき
-                {
-                    //Debug.Log("壁がある");
-                    this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.PATROL);
-
-                }
                 if (hit.collider.tag == "Player")   //プレイヤーまで障害物がないとき
                 {
                                                                                   //  GetComponent<NavMeshAgent>().isStopped = false;               //ナビゲーションを使う
                     //Debug.Log("追っかけるよー");
                     this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.TRACKING);        //Enemyの行動パターンの変更　パトロールのパターンに変更
                     this.GetComponent<Enemy>().SetDestination(Player.transform.position);
-                    //Player_Nav.SetDestination(Player.transform.position);       //ナビゲーションの目標座標 SetEnemyType(ENEMY_TYPE type)を呼び出す処理に変える
+                    return;
                 }
             }
-            else//  目の前になにもないとき
-            {
-                //Debug.Log("探すよー");
-                this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.PATROL);
-            }
+
         }
-        else
+
+        if(this.GetComponent<Enemy>().GetEnemyType() != Enemy.ENEMY_TYPE.PEPPER)
         {
-            //Debug.Log("何も見てないよ");
             this.GetComponent<Enemy>().SetEnemyType(Enemy.ENEMY_TYPE.PATROL);
         }
+
     }
+
+
 }
---
+++ futurecreate/Assets/sasaki/sasaki/spritchange.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spritchange : MonoBehaviour
+{
+    private Image m_Image;
+    public Sprite[] m_Sprite;
+    int cardchange = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Image = GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スペースキーが押された場合
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (cardchange >= 4)
+                cardchange = 0;
+            m_Image.sprite = m_Sprite[cardchange];
+            cardchange++;
+            //Debug.Log(cardchange);
+        }
+    }
+
+    int Cardnumber()
+    {
+        return cardchange;
+    }
+
+}
