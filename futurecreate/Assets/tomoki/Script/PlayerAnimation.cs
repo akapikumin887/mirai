1eee83c6d2e78a313b6563cb2bbfee13ca77bbda
aka <akapikumin232@gmail.com>
2021-12-09 09:58:18 +0900

    ともき : ステージ1作成

---
+++ futurecreate/Assets/tomoki/Script/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator _Animator;
+
+    void Start()
+    {
+        _Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
