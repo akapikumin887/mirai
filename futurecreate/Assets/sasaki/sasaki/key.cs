8f58f997a70a35df3a9e1b020c2fccf24e3f0711
razuberikku <50733194+razuberikku@users.noreply.github.com>
2021-12-16 12:31:47 +0900

    Revert "かぎ"
    This reverts commit fe1067f01725c82f147809a99857e86c86c4fb57.

--- futurecreate/Assets/sasaki/sasaki/key.cs
+++ futurecreate/Assets/sasaki/sasaki/key.cs
@@ -67,31 +67,19 @@ public class key : MonoBehaviour
         //    }
         //}
     }
-    void OnTriggerStay(Collider other)
-    { 
-        Debug.Log("atatta");
-        //接触しているオブジェクトのタグが"Player"のとき
-        if (other.CompareTag("Player"))
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == ("key") )
         {
-            Destroy(this.gameObject);
-            ////オブジェクトの色を赤に変更する
-            //GetComponent<Renderer>().material.color = Color.red;
+            Destroy(collision.gameObject);
+            Getkey1();
         }
+
+        if (collision.gameObject.tag == ("door") && key_code1 == true)
+            Destroy(collision.gameObject);
+
     }
-
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    Debug.Log("atatta");
-    //    if (collision.gameObject.tag == ("key"))
-    //    {
-    //        Destroy(collision.gameObject);
-    //        Getkey1();
-    //    }
-
-    //    if (collision.gameObject.tag == ("door") && key_code1 == true)
-    //        Destroy(collision.gameObject);
-
-    //}
 
 
     void Getkey1()
