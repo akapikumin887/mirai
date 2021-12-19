4237e3b327fd2c1c2a8e54e93607cbbd53763642
totsu999 <nixyt999@gmail.com>
2021-12-19 17:15:41 +0900

    時計慣性

---
+++ futurecreate/Assets/totsu/script/clock.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock : MonoBehaviour
+{
+    [SerializeField] GameObject Bell; //音オブジェクト
+    [SerializeField] float IdlingTime = 5.0f;
+    [SerializeField] float Size = 100.0f;
+    [SerializeField] float RingingTime = 5.0f;
+
+
+    private float frame = 0.0f;
+    private bool Ring = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        frame += Time.deltaTime;
+
+        //一定時間経過後に鳴らす
+        if (frame >= IdlingTime && !Ring)
+        {
+            GameObject bell = Instantiate(Bell, transform.position, Quaternion.identity) as GameObject;
+            //スクリプトオブジェクトを取得
+            ring b = bell.GetComponent<ring>();
+            //音の設定(規模、なっている時間(f),タグ)
+            b.SetBell(Size, RingingTime, "Bell");
+        }
+
+        if(frame >= IdlingTime + RingingTime)
+        {
+            Destroy(this);
+        }
+    }
+}
