4fa15955f3350e60ee2c571bf3e70d5829f19691
totsu999 <nixyt999@gmail.com>
2021-12-24 13:26:31 +0900

    ランプ修正

--- futurecreate/Assets/totsu/script/Lamp.cs
+++ futurecreate/Assets/totsu/script/Lamp.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Lamp : MonoBehaviour
 {
-    [SerializeField] DoorController door;
+    DoorController door;
     private bool doorLock = true;      //ドアのロック状況を取得するフラグ
-    private bool f_doorLock = false;    //色変更フラグ
 
     [SerializeField] GameObject OKLight;
 
     // Start is called before the first frame update
     void Start()
     {
+        door = transform.parent.GetComponent<DoorController>();
         doorLock = door.GetDoorLock();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!f_doorLock
-            && !doorLock)
+        doorLock = door.GetDoorLock();
+        if(!doorLock)
         {
             //f_doorLock = true;
             ////緑色に変更する
