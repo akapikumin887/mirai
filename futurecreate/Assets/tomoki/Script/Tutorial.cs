673dd06c84d57e877d6dd7ef5bb0559b73ef357f
shiro <mcagn@ahk.jp>
2021-12-24 19:18:28 +0900

    チュートリアル作成

--- futurecreate/Assets/tomoki/Script/Tutorial.cs
+++ futurecreate/Assets/tomoki/Script/Tutorial.cs
@@ -2,18 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Tutorial : MonoBehaviour
 {
-    public Image image { set; get; }
-
+    private GameObject Image1;
+    private GameObject Image2;
+    private int num;
     void Start()
     {
-        
+        Image1 = transform.GetChild(0).gameObject;
+        Image2 = transform.GetChild(1).gameObject;
+        num = 0;
     }
 
     void Update()
     {
-        
+        if (Keyboard.current.dKey.wasPressedThisFrame ||
+           Keyboard.current.aKey.wasPressedThisFrame ||
+           Keyboard.current.rightArrowKey.wasPressedThisFrame ||
+           Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            if (num == 0)
+            {
+                Image1.SetActive(false);
+                Image2.SetActive(true);
+            }
+            else if (num == 1)
+            {
+                Image2.SetActive(false);
+                scene_manager.FadeOut(0);
+            }
+            num++;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            if (num == 0)
+            {
+                Image1.SetActive(false);
+                Image2.SetActive(true);
+            }
+            else if (num == 1)
+            {
+                Image2.SetActive(false);
+                scene_manager.FadeOut(0);
+            }
+            num++;
+        }
     }
 }
