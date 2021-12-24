a7c2f06862ffbf17f0031cb4feefa0e999f7c11f
aka <akapikumin232@gmail.com>
2021-12-24 19:40:38 +0900

    時計にもUIつけた

--- futurecreate/Assets/totsu/Object/clock/clockget.cs
+++ futurecreate/Assets/totsu/Object/clock/clockget.cs
@@ -7,19 +7,21 @@ public class clockget : MonoBehaviour
 {
     [SerializeField] private int _clockNum;
     [SerializeField] private GameObject notificationUI;
-    [SerializeField] private GameObject findUI;
     private GameMng _Script;
 
     private bool _FirstTime;    //始めてだったらここをtrueにする
 
     [SerializeField] private AudioClip clip;
     private AudioSource audioSource;
+    private ItemUI _ItemUI;
+
 
     void Start()
     { 
         //Componentを取得
         audioSource = GetComponent<AudioSource>();
         _Script = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameMng>();
+        _ItemUI = _Script._Pleyer.transform.GetChild(5).GetComponent<ItemUI>();
     }
 
     // Update is called once per frame
@@ -33,20 +35,25 @@ public class clockget : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             //UIの表示
-
+            _ItemUI._IsDraw = true;
 
             //KEYを押したらアイテム入手
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-
                 audioSource.PlayOneShot(clip);
                 Destroy(this.gameObject);
                 _Script._ClockCount++;
                 Notification nof = notificationUI.GetComponent<Notification>();
-                nof.CallNotification("目覚まし時計　を入手した");
-
+                nof.CallNotification("目覚まし時計　　を入手した");
+                _ItemUI._IsDraw = false;
             }
 
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _ItemUI._IsDraw = false;
+    }
+
 }
